2440496815cb51ca9cfdd50baeb10be110e093da
Garrett <garrettleegrahamanderson@gmail.com>
2023-09-21 20:02:24 -0400

    added new logo image. adding html and styling for the about, offering, training rates and testimonials sections

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./assets/images/BernhardtPerformance_Full_RGB.png";
+import logo from "./assets/images/BernhardtPerformance_Wordmark_RGB.png";
 import video from "../src/assets/videos/bernhardt-performance.MOV";
 import "./App.css";
 
@@ -11,10 +11,10 @@ function App() {
         </video>
         <section className="overlay">
           <img src={logo} />
-          <h3>A customized program to meet your unique situation</h3>
+          <h1>A customized program to meet your unique situation</h1>
         </section>
       </header>
-      <section>
+      <section className="about-section">
         <img src={logo} />
         <section>
           <h1>About</h1>
@@ -26,14 +26,12 @@ function App() {
             playing ability, and help keep me healthy and injury free throughout
             my athletic career.
           </p>
-          <br />
           <p>
             In addition to my training, i committed to playing volleyball at
             Gardner-Webb University where I earned a B.S. in Exercise Science.
             Since Graduating from GWU, I’ve been working in the health and
             fitness industry for about six years.
           </p>
-          <br />
           <p>
             During my professional career in the industry, I’ve discovered that
             my greatest interest is in working with athletes and helping them
@@ -41,6 +39,65 @@ function App() {
             proper nutrition, up-to-date recovery methods, and mentorship.
           </p>
         </section>
+      </section>
+      <section className="offering-section">
+        <p>
+          I started Bernhardt Performance in 2021 to materialize this interest.
+          If you’re looking to reach your full potential, then Bernhardt
+          Performance is the place for you.
+        </p>
+        <article>
+          <h1>Offering</h1>
+          <ol>
+            <li>Weight Loss/ Management</li>
+            <li>Toning & Sculpting</li>
+            <li>Resistance Training</li>
+            <li>Muscle Building</li>
+            <li>Mobility / Flexibility</li>
+            <li>Planning & Periodisation</li>
+            <li>Speed, Strength, Power Development</li>
+            <li>Nutritional Advice</li>
+          </ol>
+        </article>
+      </section>
+      <section className="training-rates-section">
+        <h1>Training Rates</h1>
+        <p>
+          An assessment fee of $30 that is waived with the purchase of a
+          package. Package of ten 1-hour training sessions is $650.
+        </p>
+      </section>
+      <section className="testimonials-section">
+        <h1>Testimonials</h1>
+        <ol>
+          <li>
+            <blockquote>
+              <p>
+                “Since training with Bernhardt Performance, I have seen
+                significant improvement in my strength, speed, and durability!
+                Coaches have commented on my improved athleticism and strength.
+                I have also been educated on proper diet and nutrition to be at
+                my peak performance.”
+              </p>
+              <footer>- Ari Smith</footer>
+            </blockquote>
+          </li>
+          <li>
+            <blockquote>
+              <p>
+                “Erin Bernhardt of Bernhardt Performance is an amazing trainer.
+                My son Ari has been training with Erin for almost a year now. We
+                drive all the way from Baltimore every week so that he can work
+                out with Erin. Ari’s speed, strength and overall mental wellness
+                has improved greatly. Erin builds a trusting working
+                relationship with her clients and works on their athletic
+                confidence as well. I would highly recommend Bernhardt
+                Performance to anyone! We love working with Erin!”
+              </p>
+              <footer>- Neka Smith</footer>
+            </blockquote>
+          </li>
+        </ol>
       </section>
     </div>
   );
